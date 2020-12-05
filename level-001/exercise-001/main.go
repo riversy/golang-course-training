74e665a87f026ae342dc57b4ade19577aba390e7
Igor Goltsov <riversy@gmail.com>
2020-12-06 00:28:34 +0300

    Add a solution for the exercise 1 of the level 1

---
+++ level-001/exercise-001/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+
+	// Use a single print statement
+	fmt.Println(x, y, z)
+
+	// Use multiple print statements
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+}
