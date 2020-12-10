2f9126733cda7c7f08cafec0a535b12af747eca5
Igor Goltsov <riversy@gmail.com>
2020-12-10 23:57:40 +0300

    Add a solution for the exercise 8 of the level 4

---
+++ level-004/exercise-008/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	favs := map[string][]string{
+		`bond_james`:      {`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: {`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           {`Being evil`, `Ice cream`, `Sunsets`},
+	}
+
+	for key, personFavs := range favs {
+		fmt.Printf(">>> Favs of the %v <<<\n", key)
+		for i, fav := range personFavs {
+			fmt.Printf("\t- %v. %v\n", i+1, fav)
+		}
+	}
+}
