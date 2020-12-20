6cb299d2803fbead1d17849317a54e1ac934077e
Igor Goltsov <riversy@gmail.com>
2020-12-21 00:08:56 +0300

    Add a solution for the exercise 6 of the level 10

---
+++ level-010/exercise-006/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	c := make(chan int)
+
+	go func() {
+		for i := 1; i <= 100; i++ {
+			c <- i
+		}
+		close(c)
+	}()
+
+	for v := range c {
+		fmt.Println(v)
+	}
+
+	fmt.Println("About to exit")
+}
